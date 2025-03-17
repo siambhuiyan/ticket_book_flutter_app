2ae1c3ee75e8e020606f124c27acd62a7538ec98
siambhuiyan <asadsiam588@gmail.com>
2025-03-17 13:29:57 +0600

    project intialize with bottom nav and home screen skeleton

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:ticket_app/base/bottom_nav_bar.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: BottomNavBar(),
+    );
+  }
+}
